1affba4e2e852ea34ec2e9d34a01eb648f34c8dc
Aleeeehh <alessioprato03@gmail.com>
2025-09-09 22:16:42 +0200

    use two motors (M1 & M4)

--- main/dshot_main.cpp
+++ main/dshot_main.cpp
@@ -12,9 +12,11 @@ extern "C" void app_main()
     esp_task_wdt_deinit();
     printf("[INFO] Task watchdog disabilitato\n");
 
-    // Inizializza ESC su GPIO 18 con protocollo DSHOT600
-    DShotRMT esc(GPIO_NUM_18, DSHOT600);
-    esc.begin();
+    // Inizializza ESC su di un GPIO con protocollo DSHOT600
+    DShotRMT M1(GPIO_NUM_18, DSHOT600); //M1
+    DShotRMT M4(GPIO_NUM_27, DSHOT600); //M4
+    M1.begin();
+    M4.begin();
     printf("[INFO] ESC inizializzato con DShot600 su GPIO 18 (motore M1)\n");
 
     int test_count = 0;
@@ -30,7 +32,8 @@ extern "C" void app_main()
         printf("[INFO] Arming ESC (2 secondi)...\n");
         for (int i = 0; i < 40000; i++)
         {
-            esc.setThrottle(0);
+            M1.setThrottle(0);
+            M4.setThrottle(0);
             if (i % 500 == 0)
             {
                 printf("[DEBUG] arming... %d ms\n", i * 2);
@@ -40,10 +43,11 @@ extern "C" void app_main()
         
 
         // Salita da 100 a 500
-        for (int throttle = 100; throttle <= 500; throttle += 5) 
+        for (int throttle = 100; throttle <= 1100; throttle += 20) 
         {
-            esc.setThrottle(throttle);
-            
+            M1.setThrottle(throttle);
+            M4.setThrottle(throttle);
+           
             //1 secondo per velocità
             for (int second = 1; second <= 1; second++)
             {
@@ -53,9 +57,10 @@ extern "C" void app_main()
         }
 
         // Discesa da 500 a 100
-        for (int throttle = 500; throttle >= 100; throttle -= 5) 
-        {
-            esc.setThrottle(throttle);
+        for (int throttle = 1100; throttle >= 100; throttle -= 20) 
+        {   
+            M1.setThrottle(throttle);
+            M4.setThrottle(throttle);
             
             //1 secondo per velocità
             for (int second = 1; second <= 1; second++)
@@ -65,9 +70,10 @@ extern "C" void app_main()
             }
         }
 
-        printf("[INFO] Fine ciclo completo. Pausa lunga 10 secondi...\n");
-        esc.setThrottle(0);
-        vTaskDelay(pdMS_TO_TICKS(10000)); // 10 secondi di pausa tra i cicli
+        printf("[INFO] Fine ciclo completo. Pausa lunga 5 secondi...\n");
+        M1.setThrottle(0);
+        M4.setThrottle(0);
+        vTaskDelay(pdMS_TO_TICKS(5000)); // 5 secondi di pausa tra i cicli
     }
 
     printf("[INFO] Test completato. Loop idle.\n");
